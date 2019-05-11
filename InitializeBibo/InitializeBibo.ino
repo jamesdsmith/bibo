908217e7c04a8fa649de472fd1baf267deece30f
James Smith <codefiend@gmail.com>
2019-05-10 20:22:16 -0700

    Proper initialize code

--- InitializeBibo/InitializeBibo.ino
+++ InitializeBibo/InitializeBibo.ino
@@ -10,19 +10,9 @@ const int PIN_2 = 6;
 const int SENSOR_PIN = 9;
 const int DIRECTION_PIN = A1;
 
-// Pulse Sensor Setup
-const int OUTPUT_TYPE = SERIAL_PLOTTER;
-const int PULSE_INPUT = A2;
-const int PULSE_BLINK = 13;
-const int PULSE_FADE = 5;
-const int THRESHOLD = 550;
-const int BPM_THRESHOLD = 100;
 const int TOTAL_TURNS = 2000;
+const int LOADED_TURNS = 200;
 
-byte samplesUntilReport;
-const byte SAMPLES_PER_SERIAL_SAMPLE = 10;
-
-PulseSensorPlayground pulseSensor;
 IRSensor ir(SENSOR_PIN);
 MotorDriver motor(PIN_1, PIN_2);
 
@@ -34,48 +24,14 @@ void motorIn() {
   motor.Stop();
 }
 
-void motorOut() {
-  motor.CW();
-  ir.waitForTicks(TOTAL_TURNS);
-  motor.Stop();
-}
-
 void setup() {
   Serial.begin(115200);
-//  motorIn();
-
-//  motorOut();
-//  motor.CCW();
-//  ir.waitForTicks(200);
-//  motor.Stop();
-//  motorIn();
-motorOut();
-
+  motor.CCW();
+  ir.waitForTicks(LOADED_TURNS);
+  motor.Stop();
+  
   motorIn();
-//  motor.CCW();
-////  2600
-//  ir.waitForTicks(2600);
-//  motor.Stop();
-////  ir.waitForTurns(1);
-////  motor.Stop();
-//  //  motor.CW();
-//  motorOut();
-//  motorIn();
 }
 
 void loop() {
-//  motorIn();
-//  motorOut();
-//  motorIn();
-  
-     
-//    ir.sample();
-//    Serial.println(ir.getTickCount());
-////    if (ir.getTurnCount() > 1) {
-////      motor.Stop();
-////      motorActive = false;
-////    }
-//
-//    delay(10);
-
 }
